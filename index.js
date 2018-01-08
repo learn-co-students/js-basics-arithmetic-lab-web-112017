7253db85ac38f6cf6289bd6efc3d1c683534d596
Flatiron School <leslie.hsu@flatironschool.com>
2018-01-08 13:18:59 -0500

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+
+let newID = oldID + 1000000000;
+
+let ageIsValid = Number.isInteger(currentAge);
+
+let randomNumber = Math.random()*10 +11;//0 inclusive - 20 exclusive
+
+let randomInteger = Math.floor(randomNumber); //rounds down to nearest integer 0 - 20 exclusive
+
+let randomUserID = randomInteger + 1 + 1000000000;
